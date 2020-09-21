4e281b8a26a25659059d8451121f365759c07c35
Yilool <20010917chen@gmail.com>
2020-09-21 11:01:49 +0200

    Subida corrección EvalInit y PractInit

--- JSPrácticaInit/YiEjercicio3.js
+++ JSPrácticaInit/YiEjercicio3.js
@@ -1,23 +1,17 @@
 const sh = document.getElementById("sh")
 
 sh.addEventListener('click', () => {
-    var prov = ["Sevilla","Cadiz","Huelva","Malaga","Granada","Almeria","Jaen","Cordoba"]
-
+    //Creo el array de las Provincias
+    var prov = ["Sevilla", "Cadiz", "Huelva", "Malaga", "Granada", "Almeria", "Jaen", "Cordoba"]
+        //Creo otro array donde almaceno el array anterior pero ordenado mediante sort
     var provSort = prov.sort()
-
-    document.innerHTML = "<table></table>"
-
-    var tab = document.getElementsByTagName("table")
-
+        //Escribo en el html la etiqueta table
+    document.write('<table border="1">')
+        //Recorro el array con forof
     for (iteProv of provSort) {
-        newTr = document.createElement("tr")
-        newTd = document.createElement("td")
-        newTx = document.createTextNode(iteProv)
-
-        newTd.appendChild(newTx)
-        newTr.appendChild(newTd)
-        tab.appendChild(newTr)
+        //Para cada elemento le creo sus tr, td.
+        document.write('<tr>' + '<td>' + iteProv + '</td>' + '</tr>')
     }
+    //Cierro la etiqueta table
+    document.write('</table>')
 })
-
-//Duda: porqué tab.appendChild no es una función
