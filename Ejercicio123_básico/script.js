da858b81fd1721e7099f945a4a08acaf199d40f8
Yilool <20010917chen@gmail.com>
2020-09-25 09:16:47 +0200

    Subida corrección eventos

--- Ejercicio123_básico/script.js
+++ Ejercicio123_básico/script.js
@@ -10,10 +10,8 @@ caja.addEventListener("mouseenter", () => {
 })
 
 caja.addEventListener("mouseleave", () => {
-        caja.style.background = 'red';
-        //caja.className = 'box'
-    })
-    //clasname no funciona pero tampoco de error??
+    caja.style.background = 'red';
+})
 
 caja.addEventListener("mousedown", () => {
     console.log("Has pulsado la caja");
@@ -29,21 +27,27 @@ campo.addEventListener("keydown", () => {
     })
 })
 
-campo.addEventListener("keyup", () => {
-        console.log("Has soltado una tecla");
+campo.addEventListener("keyup", (e) => {
+        console.log("Has soltado una tecla: " + e.key);
     })
     //String.fromCharCode(num1, num2, ..., numN); => para saber qué tecla ha sido presionado
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
 })
+
 bot.addEventListener("click", () => {
-    campForm.addEventListener("keypress", () => {
-        console.log(String.fromCharCode(event.keyCode));
+    campForm.addEventListener("keyup", (e) => {
+        console.log(e.key);
     })
 })
 
 galeria.addEventListener("click", (e) => {
-    let item = e.target.textContent
-    item.style.background = 'red'
+    let item = e.target
+
+    if (item.className == 'gallery__item') {
+        item.className = 'gallery__item_2'
+    } else {
+        item.className = 'gallery__item'
+    }
 })
