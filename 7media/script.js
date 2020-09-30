3729d80c17ad7c083b32440ad703c8482a030f28
Yilool <20010917chen@gmail.com>
2020-09-30 09:39:49 +0200

    Subida Ejercicio 7media acabado

--- 7media/script.js
+++ 7media/script.js
@@ -1,59 +1,94 @@
+//Obtengo del html los 3 elementos a añadir eventos
 const traseraJugador = document.getElementById("jug")
 const reiniciar = document.getElementById("reiniciar")
 const plantar = document.getElementById("plantar")
+
+//Array de los palos, otro de las cartas ya sacada y los puntos
 const palos = ['Bastos', 'Copas', 'Espadas', 'Oros']
 const cartasUsado = new Array()
 let pointPlayer = 0
 let pointBank = 0
 
+//Al clikear la trasera del jugador...
 traseraJugador.addEventListener('click', () => {
-    let palo = getPalos()
-    let valor = getValores()
+    let repetido = false;
 
-    cartasUsado.push(`${valor}${palos[palo]}.jpg`)
-    document.getElementById("playerObtenido").innerHTML += `<img class="cart" src="imagenes/${valor}${palos[palo]}.jpg">`
+    do {
+        //genera un palo(0-3) y un valor(1-10)
+        let palo = getPalos()
+        let valor = getValores()
 
-    pointPlayer += sumaPuntos(valor)
+        //Comprueba si existe dicha carta el el array obtenido sino se le añade y se muestra en la colleccion de cartas del jugador
+        if (cartasUsado.includes(`${valor}${palos[palo]}.jpg`) == true) {
+            repetido = true
+        } else {
+            cartasUsado.push(`${valor}${palos[palo]}.jpg`)
+            document.getElementById("playerObtenido").innerHTML += `<img class="cart" src="imagenes/${valor}${palos[palo]}.jpg">`
 
-    document.getElementById("jugadorPunto").innerHTML = `Jugador: ${pointPlayer}`
+            pointPlayer += sumaPuntos(valor)
 
+            document.getElementById("jugadorPunto").innerHTML = `Jugador: ${pointPlayer}`
+        }
+    } while (repetido == true);
+
+    //Si el punto del jugador se pasa del 7.5 se actualiza la pagina en 0.5s y se deshabilita el boton plantar
     if (pointPlayer > 7.5) {
         document.getElementById("boton").innerHTML += `Has perdido`
         plantar.disabled = true
+        setTimeout(actualizar, 500)
     }
 })
 
-plantar.addEventListener('click', setTimeout(() => {
-    () => {
+plantar.addEventListener('click', () => {
+    //Esto se repite hasta que los puntos de la banca supere el 7
+    do {
+        //esto se repite si encuantra una carta repetida
         let repetido = false;
         do {
-            do {
-                let palo = getPalos()
-                let valor = getValores()
+            //genera un palo(0-3) y un valor(1-10)
+            let palo = getPalos()
+            let valor = getValores()
 
-                if (cartasUsado.includes(`${valor}${palos[palo]}.jpg`) == true) {
-                    repetido = true
-                } else {
-                    cartasUsado.push(`${valor}${palos[palo]}.jpg`)
-                    document.getElementById("bancaObtenido").innerHTML += `<img class="cart" src="imagenes/${valor}${palos[palo]}.jpg">`
+            //Comprueba si existe dicha carta el el array obtenido sino se le añade y se muestra en la colleccion de cartas de la banca
+            if (cartasUsado.includes(`${valor}${palos[palo]}.jpg`) == true) {
+                repetido = true
+            } else {
+                cartasUsado.push(`${valor}${palos[palo]}.jpg`)
+                document.getElementById("bancaObtenido").innerHTML += `<img class="cart" src="imagenes/${valor}${palos[palo]}.jpg">`
 
-                    pointBank += sumaPuntos(valor)
+                pointBank += sumaPuntos(valor)
 
-                    document.getElementById("bancaPunto").innerHTML = `Banca: ${pointBank}`
-                }
-            } while (pointBank < 7);
-        } while (repetido == false && pointBank < 7.5)
+                document.getElementById("bancaPunto").innerHTML = `Banca: ${pointBank}`
+            }
+        } while (repetido == true);
+    } while (pointBank < 7)
+
+    //Si el punto del jugador en mayor gana el jugador
+    if (pointPlayer > pointBank || pointBank > 7.5) {
+        document.getElementById("boton").innerHTML += `Has ganado`
+    } else {
+        document.getElementById("boton").innerHTML += `Gana banca`
     }
-}, 1))
 
+    //Actualiza dentro de 0.5s
+    plantar.disabled = true
+    setTimeout(actualizar, 500)
+})
+
+//Al clikear el boton de reiniciar llama a la funcion actualizar
+reiniciar.addEventListener('click', actualizar)
+
+//Funcion para obtener un numero aleatorio del 0-3 (Hay 4 tipos de palos)
 function getPalos() {
     return Math.floor(Math.random() * ((3 + 1) - 0) + 0)
 }
 
+//Funcion para obtener un numero aleatorio del 1-10 ( hay 10 cartas por palos)
 function getValores() {
     return Math.floor(Math.random() * ((10 + 1) - 1) + 1)
 }
 
+//Funcion para asignar los puntos: los mayores de 7 son 0.5 puntos y los otros su valor
 function sumaPuntos(valor) {
     let point = 0
 
@@ -64,4 +99,9 @@ function sumaPuntos(valor) {
     }
 
     return point
+}
+
+//Funcion actualizar: recarga la pagina
+function actualizar() {
+    location.reload()
 }
